049fd5fcdb25f128b59d0d0b37772ffbf49e473e
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2018-09-11 13:14:03 +0000

    Remove some dated bits

--- src/Erl/Cowboy/Handler.purs
+++ src/Erl/Cowboy/Handler.purs
@@ -1,17 +1,14 @@
 module Erl.Cowboy.Handler where
 
 
-import Control.Monad.State (StateT, evalStateT)
+import Prelude
+
+import Control.Monad.State (StateT, runStateT)
+import Data.Tuple (uncurry)
 import Effect (Effect)
 import Erl.Cowboy.Req (Req)
 
-data Ok a = Ok Req a
-
-type Handler a = Req -> a -> Ok a
-
-type EffectHandler a = Req -> a -> Effect (Ok a)
-
-runHandlerM :: forall res. HandlerM res -> Req -> Effect res
-runHandlerM h req = evalStateT h req 
-
 type HandlerM = StateT Req Effect
+
+runHandlerM :: forall a b. (a -> Req -> b) -> HandlerM a -> Req -> Effect b
+runHandlerM res h req = uncurry res <$> runStateT h req
